5f76640b65645d4074657bac6971301df391690f
Pihkaal <pihkaal@proton.me>
2024-12-02 14:46:33 +0100

    feat: display palette

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,150 @@ struct Pixel {
     filled: bool,
 }
 
+struct Palette {
+    colors: [RGB; 20],
+}
+
+struct RGB {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl RGB {
+    fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+
+    fn to_color(&self) -> Color {
+        Color::Rgb {
+            r: self.r,
+            g: self.g,
+            b: self.b,
+        }
+    }
+}
+
+impl Palette {
+    fn new() -> Self {
+        Self {
+            colors: [
+                RGB::new(3, 104, 63),
+                RGB::new(37, 19, 190),
+                RGB::new(242, 55, 94),
+                RGB::new(123, 223, 67),
+                RGB::new(45, 78, 205),
+                RGB::new(200, 120, 14),
+                RGB::new(75, 192, 203),
+                RGB::new(145, 20, 145),
+                RGB::new(56, 34, 89),
+                RGB::new(244, 67, 54),
+                RGB::new(77, 99, 232),
+                RGB::new(188, 65, 101),
+                RGB::new(96, 231, 79),
+                RGB::new(222, 190, 75),
+                RGB::new(105, 135, 90),
+                RGB::new(10, 220, 182),
+                RGB::new(200, 40, 60),
+                RGB::new(90, 24, 180),
+                RGB::new(50, 200, 50),
+                RGB::new(255, 165, 0),
+            ],
+        }
+    }
+
+    fn get_color(&self, index: u8) -> Color {
+        self.colors[index as usize].to_color()
+    }
+
+    fn count(&self) -> u8 {
+        self.colors.len() as u8
+    }
+
+    fn render(&self) -> io::Result<()> {
+        let mut stdout = io::stdout();
+        let size = terminal::size()?;
+
+        let draw_cell = |x: u16, y: u16, color_index: u8| -> io::Result<()> {
+            let color = &self.colors[color_index as usize];
+            let brightness =
+                (0.299 * color.r as f32 + 0.587 * color.g as f32 + 0.114 * color.b as f32) / 255.0;
+
+            let mut stdout = io::stdout();
+
+            queue!(
+                stdout,
+                style::SetForegroundColor(Color::Black),
+                style::SetBackgroundColor(color.to_color()),
+                cursor::MoveTo(x, y),
+                style::Print("█🬂🬂🬂🬂🬂🬂█"),
+                cursor::MoveTo(x, y + 1),
+                style::Print("█      █"),
+                cursor::MoveTo(x, y + 2),
+                style::Print("█🬭🬭🬭🬭🬭🬭█"),
+                style::ResetColor
+            )?;
+
+            queue!(
+                stdout,
+                cursor::MoveTo(x + 3, y + 1),
+                style::SetBackgroundColor(color.to_color()),
+                style::SetForegroundColor(if brightness > 0.5 {
+                    Color::Black
+                } else {
+                    Color::White
+                }),
+                style::Print(format!("{: >2}", color_index + 1)),
+                style::ResetColor,
+            )?;
+
+            Ok(())
+        };
+
+        const CELL_WIDTH: u16 = 8;
+        const CELL_HEIGHT: u16 = 3;
+        const CELLS_PER_ROW: u8 = 5;
+        const CELLS_PER_COL: u8 = 2;
+        const TOTAL_WIDTH: u16 = CELL_WIDTH * CELLS_PER_ROW as u16;
+
+        let x = (size.0 - TOTAL_WIDTH) / 2;
+        let y = size.1 - 2 * CELL_HEIGHT - 2;
+
+        queue!(
+            stdout,
+            cursor::MoveTo(x, y),
+            style::SetForegroundColor(Color::Black),
+            style::Print("🬭".repeat(TOTAL_WIDTH as usize))
+        )?;
+
+        for row in 0..CELLS_PER_COL {
+            for col in 0..CELLS_PER_ROW {
+                draw_cell(
+                    x + col as u16 * CELL_WIDTH,
+                    y + row as u16 * CELL_HEIGHT + 1,
+                    row * CELLS_PER_ROW + col,
+                )?;
+            }
+        }
+
+        queue!(
+            stdout,
+            cursor::MoveTo(x, 1 + y + 2 * CELL_HEIGHT),
+            style::SetForegroundColor(Color::Black),
+            style::Print("🬂".repeat(TOTAL_WIDTH as usize)),
+            style::ResetColor
+        )?;
+
+        Ok(())
+    }
+}
+
 struct Board {
     x: i16,
     y: i16,
 
-    pixels: [[Pixel; 10]; 10],
-    palette: [Color; 3],
+    pixels: [[Pixel; 75]; 75],
+    palette: Palette,
 }
 
 impl Board {
@@ -39,8 +177,8 @@ impl Board {
             pixels: [[Pixel {
                 color: 0,
                 filled: false,
-            }; 10]; 10],
-            palette: [Color::Red, Color::Green, Color::Blue],
+            }; 75]; 75],
+            palette: Palette::new(),
         }
     }
 
@@ -87,7 +225,7 @@ impl Board {
                 if pixel.filled {
                     queue!(
                         stdout,
-                        style::SetBackgroundColor(self.palette[pixel.color as usize]),
+                        style::SetBackgroundColor(self.palette.get_color(pixel.color)),
                         style::Print("  "),
                         style::ResetColor
                     )?;
@@ -144,7 +282,7 @@ fn main() -> io::Result<()> {
     // randomize board
     for row in 0..board.height() {
         for col in 0..board.width() {
-            let random_color = rand::random::<u8>() % 3;
+            let random_color = rand::random::<u8>() % board.palette.count();
             board.set(
                 col,
                 row,
@@ -189,7 +327,7 @@ fn main() -> io::Result<()> {
                         }
                     }
                     MouseEventKind::Down(MouseButton::Right) => {
-                        current_color = (current_color + 1) % (board.palette.len() as u8);
+                        current_color = (current_color + 1) % board.palette.count();
                     }
                     MouseEventKind::Down(MouseButton::Middle) => {
                         drag_start = (event.column, event.row);
@@ -213,6 +351,7 @@ fn main() -> io::Result<()> {
         )?;
 
         board.render()?;
+        board.palette.render()?;
 
         queue!(
             stdout,
